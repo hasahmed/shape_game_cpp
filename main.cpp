ca6e8417f5b4eb0f964f520034d13887aff5d0ae
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-03-23 02:21:27 -0400

    mostly made code that tracks fps

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #define GLFW_INCLUDE_GLCOREARB
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <chrono>
 #include "GLHandler.hpp"
 #include "Window.hpp"
 //#include "main.hpp"
+
+typedef std::chrono::high_resolution_clock Clock;
+
 
 const char *vertex_shader =
 "#version 410 core\n"
@@ -25,6 +29,12 @@ const char *fragment_shader =
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE)
          glfwSetWindowShouldClose(window, true);
+    if (key == GLFW_KEY_1)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (key == GLFW_KEY_2)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     //std::cout << key << ":" << scancode << ":" << action << ":" << mods << std::endl;
 }
 void check_shader_err(int shader){
@@ -117,33 +127,40 @@ int main() {
     //set up vertext data
     //------------------------------------------------------------------------
     float color[] = {1.0, 1.0, 0.0, 1.0};
-    float points[] = {
-        -0.5f, -0.5f, 0.0f, // left
-        0.5f, -0.5f, 0.0f, // right
-        0.0f,  0.5f, 0.0f  // top
-    };
-
     //float points[] = {
-        //0.5f,  0.5f, 0.0f,  // top right
-        //0.5f, -0.5f, 0.0f,  // bottom right
-        //-0.5f, -0.5f, 0.0f,  // bottom left
-        //-0.5f,  0.5f, 0.0f   // top left
-    //};
-    //unsigned int indices[] = {  // note that we start from 0!
-        //0, 1, 3,   // first triangle
-        //1, 2, 3    // second triangle
+        //-0.5f, -0.5f, 0.0f, // left
+        //0.5f, -0.5f, 0.0f, // right
+        //0.0f,  0.5f, 0.0f  // top
     //};
 
+    float points[] = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
 
 
 
     //vbo
-    GLuint vbo, vao;
+    GLuint vbo, vao, ebo;
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
     glBindVertexArray(vao);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_DYNAMIC_DRAW); //dynamic because it will be modified often and updated often
+
+    //-----------------------------------------ebo
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //<-----------------------------------------ebo
+
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0); //this is actually an unbinding
@@ -171,19 +188,42 @@ int main() {
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+    int fps = 0;
+    double second_count = 0;
     while (!glfwWindowShouldClose(window)) {
+        auto t1 = Clock::now();
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(vao);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_DYNAMIC_DRAW);
 
-        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        //ebo
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+
+
+        //non ebo
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        //glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_DYNAMIC_DRAW);
+
         //update other events like input handling
         glfwPollEvents();
         // put the stuff we've been drawing onto the display
+        glfwSwapInterval(1);
         glfwSwapBuffers(window);
+        auto t2 = Clock::now();
+        std::chrono::duration<double> elapsed_seconds = t2 - t1;
+        second_count += elapsed_seconds.count();
+        //std::cout << second_count << std::endl;
+        fps++;
+        //std::cout << "delta: " << elapsed_seconds.count() << std::endl;
+        if (second_count >= 1.0) {
+            fps = 0;
+            second_count = 0;
+        }
+        //std::cout << "delta: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() << std::endl;
     }
 
     glfwTerminate();
