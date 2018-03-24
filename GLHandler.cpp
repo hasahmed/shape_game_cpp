7e14256d307a3965d2c1d0bf62fa2b4d8603ced1
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-03-24 18:10:42 -0400

    everything works up to this point

--- GLHandler.cpp
+++ GLHandler.cpp
@@ -69,5 +69,6 @@ shapegame::GLHandler::GLHandler() {
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glClearColor(0.0f, 1.0f, 1.0f, 0.5f);
 
 }
--- Window.cpp
+++ Window.cpp
@@ -11,6 +11,7 @@ shapegame::Window::Window(int width, int height, std::string window_title) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4); //anti-aliasing
 
 
     /* Create a windowed mode window and its OpenGL context */
