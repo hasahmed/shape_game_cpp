748c540ec1f038f01735c6dcd0ac8f354f859a68
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-08-19 05:08:26 -0400

    gave up on make files and just wrote a quick ruby script. Today has been a failure as far as programming goes. Better luck in a week or so with new spirits inshallah

---
+++ FileUtil.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "FileUtil.hpp"
+
+std::vector<std::string> shapegame::FileUtil::read_file(std::string path) {
+    //the file path is built here. note it doesn't support windows. Perhpas change later with boost or homeroll
+    std::string file_path(path); //make the path platform agnostic someday
+    std::ifstream file(file_path);
+    std::vector<std::string> file_buff;
+    file_buff.push_back(std::string());
+    if (file.is_open()) {
+        int i = 0;
+        while (std::getline(file, file_buff[i])) {
+            file_buff.push_back(std::string());
+            i++;
+        }
+
+        file.close();
+    }
+    // the count of how many characters are in the vector to know how big to make the buffer
+    return file_buff;
+}
+
+std::string shapegame::FileUtil::line_vect_to_str(std::vector<std::string> str_vect) {
+    std::string concatinated_file;
+    for (std::string line : str_vect) {
+        if (!line.empty())
+            concatinated_file += (line + "\n");
+    }
+    return concatinated_file;
+}
