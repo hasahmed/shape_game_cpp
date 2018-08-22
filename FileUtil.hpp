4c0ba933413bc5bf5d9378656103673088b3b176
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-08-22 02:08:12 -0400

    removed unneeded functionality from FileUtil

--- FileUtil.hpp
+++ FileUtil.hpp
@@ -7,7 +7,6 @@ namespace shapegame {
 
     class FileUtil {
         public:
-        static std::vector<std::string> read_file(std::string path);
-        static std::string line_vect_to_str(std::vector<std::string> str_vect);
+        static std::string read(std::string path);
     };
 }
