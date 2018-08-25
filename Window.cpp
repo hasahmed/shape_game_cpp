89ac3ec6fa086805026a896dd99aff50565b153b
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-08-25 14:11:53 -0400

    added some stuff to the learning file

--- Window.cpp
+++ Window.cpp
@@ -7,8 +7,8 @@ shapegame::Window::Window(int width, int height, std::string window_title) {
     if (!glfwInit())
         throw std::runtime_error("glfw failed to initilize");
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_SAMPLES, 4); //anti-aliasing
