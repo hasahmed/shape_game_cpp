4d3e0e5c1d3fd88ebc8349cc86cadfc0b716f764
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-08-26 19:12:48 -0400

    added shapegame header

---
+++ shapegame.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __MACH__
+#include <glad/glad.h>
+#endif
+#define GLFW_DLL
+#define GLFW_INCLUDE_GLCOREARB
+#include <GLFW/glfw3.h>
+#include "Globals.hpp"
+#include "FileUtil.hpp"
+#include "Window.hpp"
+#include "Globals.hpp"
+#include "GLHandler.hpp"
+#include "ShaderHandler.hpp"
