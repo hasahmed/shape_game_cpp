03a1136616ac92a9a0a3386ddd4c477a5222bc31
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-09-06 21:55:18 -0400

    added several static colors

--- Color.cpp
+++ Color.cpp
@@ -6,5 +6,13 @@ shapegame::Color::Color(float r, float g, float b, float a) {
     _color[2] = b;
     _color[3] = a;
 }
-
 shapegame::Color::Color(float r, float g, float b) : Color(r, g, b, 1.0) {}
+
+shapegame::Color shapegame::Color::BLACK = Color(0.0, 0.0, 0.0);
+shapegame::Color shapegame::Color::WHITE = Color(1.0, 1.0, 1.0);
+shapegame::Color shapegame::Color::RED = Color(1.0, 0.0, 0.0);
+shapegame::Color shapegame::Color::GREEN = Color(0.0, 1.0, 0.0);
+shapegame::Color shapegame::Color::BLUE = Color(0.0, 0.0, 1.0);
+shapegame::Color shapegame::Color::YELLOW = Color(1.0, 1.0, 0.0);
+shapegame::Color shapegame::Color::PURPLE = Color(1.0, 0.0, 1.0);
+
--- main.cpp
+++ main.cpp
@@ -16,8 +16,8 @@ int main() {
     //Your game logic here
     // ...
     //shapegame::GameObject x;
-    auto x = shapegame::Triangle(10, 10, 10, 10, Color(1.0, 0.0, 0.0));
-    auto y = shapegame::Triangle(10, 10, 10, 10, Color(1.0, 1.0, 0.0));
+    auto x = shapegame::Triangle(10, 10, 10, 10, Color::PURPLE);
+    auto y = shapegame::Triangle(10, 10, 10, 10, Color::BLACK);
 
     y._verts[0] = -1.0;
     y._verts[1] = 1.0;
