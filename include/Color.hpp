65e4d90fa768afcfd632425719e9c408e82da6bf
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-09-06 21:55:18 -0400

    added several static colors

--- include/Color.hpp
+++ include/Color.hpp
@@ -1,12 +1,21 @@
 #pragma once
+#include "Scene.hpp"
 namespace shapegame {
     class Color {
+        friend class Scene;
         private:
-            //float _color[4];
-        public:
             float _color[4];
+        public:
             Color(float r, float g, float b, float a);
             Color(float r, float g, float b);
+
+            static Color BLACK;
+            static Color WHITE;
+            static Color RED;
+            static Color GREEN;
+            static Color BLUE;
+            static Color YELLOW;
+            static Color PURPLE;
     };
 }
 
