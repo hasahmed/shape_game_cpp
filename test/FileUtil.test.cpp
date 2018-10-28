cbdac0e83756aef6b70f302ef5bac7fc1770b65f
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-10-28 04:08:41 -0400

    added cmake as the build system for the test folder

--- test/FileUtil.test.cpp
+++ test/FileUtil.test.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -5,9 +6,34 @@
 #include "../shapegame"
 #include "FileUtil.hpp"
 
+//this mimics the data found in
+// dummy-data/lines.txt
+const char *strings = {
+  "0\n"
+  "1\n"
+  "\n"
+  "blank ^\n"
+  "2\n"
+  "3\n"
+  "4\n"
+  "5\n"
+  "6\n"
+  "7\n"
+  "8\n"
+  "9\n"
+  "10\n"
+  "\n"
+  "\n"
+  "^ two blanks\n"
+  "one blank below\n"
+  "\n"
+};
+
 int main(){
-  shapegame::FileUtil::read("dummy-data/lines.txt");
-    // shapegame::FileUtil::read("../shaders/default.frag");
-    //std::string str = shapegame::FileUtil::read("../shaders/default.frag");
-    //std::cout << str;
+  std::string str = shapegame::FileUtil::read("dummy-data/lines.txt");
+  // assert(str[0] == '0');
+  for (int i = 0; i < str.size(); i++) {
+      assert(std::string(1, str[i]).compare(std::string(1, strings[i])) == 0);
+  }
+  std::cout << "FileUtil.cpp Tests Passed" << std::endl;
 }
