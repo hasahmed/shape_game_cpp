661a20c5fee4843dcebc6813f88643a2aa86035c
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-11-25 13:39:41 -0500

    deleted some comments

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -6,19 +6,12 @@ namespace shapegame {
 			int milliseconds;
 			bool repeates;
 			bool autoStart;
-			// std::function<void()> callBack;
 			std::function<void(void)> callBack;
-			// void (Object::*callBack)(void);
-
-			// Object &callingInst;
 
 			// internal state	
 			bool _running = false;
-			// int _timeLeft;
 			std::chrono::time_point<std::chrono::high_resolution_clock> _timerStart;
 			std::chrono::duration<double> _timeElapsed;
-			// std::chrono::time_point<std::chrono::high_resolution_clock> _end;
-			// std::chrono::time_point<std::chrono::high_resolution_clock> _timerLeft;
 
 		public:
 			Timer(double seconds, bool repeates, bool autoStart, std::function<void(void)> callBack);
