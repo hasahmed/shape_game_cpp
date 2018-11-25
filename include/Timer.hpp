d3300d3ab5e729f7f6730c39f7afe336f8821fdf
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-11-25 03:50:42 -0500

    failed to use timer

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -6,7 +6,11 @@ namespace shapegame {
 			int milliseconds;
 			bool repeates;
 			bool autoStart;
-			std::function<void()> callBack;
+			// std::function<void()> callBack;
+			// std::function<void(void)> callBack;
+			void (Object::*callBack)(void);
+
+			Object &callingInst;
 
 			// internal state	
 			bool _running = false;
@@ -17,7 +21,9 @@ namespace shapegame {
 			// std::chrono::time_point<std::chrono::high_resolution_clock> _timerLeft;
 
 		public:
-			Timer(int milliseconds, bool repeates, bool autoStart, std::function<void()> callBack);
+			// Timer(int milliseconds, bool repeates, bool autoStart, std::function<void(void)> callBack);
+			Timer(int milliseconds, bool repeates, bool autoStart, void (Object::*callBack)(void), Object& callingInst);
+			// Timer(int milliseconds, bool repeates, bool autoStart, void (HeadNode::*callBack)(void), HeadNode& callingInst);
 			void start();
 			void stop();
 			void onAdd() override;
