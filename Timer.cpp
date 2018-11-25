230988291ac9f1a964eb28bbb1751f3efc3aeeed
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-11-25 02:53:23 -0500

    finished timer class enough to make it work

--- Timer.cpp
+++ Timer.cpp
@@ -1,9 +1,36 @@
+#include <chrono>
 #include "shapegame"
 using namespace shapegame;
 
-Timer::Timer() : Object() {}
+Timer::Timer(
+	int milliseconds,
+	bool repeates,
+	bool autoStart,
+	std::function<void()> callBack
+) : Object(), milliseconds(milliseconds), repeates(repeates), autoStart(autoStart), callBack(callBack) {}
 void Timer::onAdd() {
-
+	if (this->autoStart) {
+		this->start();
+	}
 }
 void Timer::update() {
+	if (this->_running) {
+		this->_timeElapsed += std::chrono::high_resolution_clock::now() - this->_timerStart;
+		if (this->_timeElapsed.count() >= milliseconds) {
+			this->callBack();
+			if (!this->repeates) {
+				puts("Need to write code to remove objects");
+				// this->destroy();
+			}
+		}
+	}
+}
+
+void Timer::start() {
+	this->_timerStart = std::chrono::high_resolution_clock::now();
+	this->_running = true;
+}
+
+void Timer::stop() {
+	this->_running = false;
 }
