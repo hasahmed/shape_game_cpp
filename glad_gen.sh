6dd0051861da443383446ee76459dd7d948f940c
Hasan Yusuf Ahmed <hasanyusufahmed@gmail.com>
2018-11-29 02:55:33 -0500

    now able to build on linux. At least fo r now

--- glad_gen.sh
+++ glad_gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-glad --out-path include/glad --spec gl --omit-khrplatform --generator c --extensions ''
+glad --out-path include/glad --spec gl --omit-khrplatform --generator c --extensions '' --api gl=4.1
