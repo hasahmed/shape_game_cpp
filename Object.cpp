8f1f7febec0e3c5669edd14bc294777a37022377
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-12-05 23:17:12 -0500

    updated make file to not try to link more than one .test.cpp file

--- Object.cpp
+++ Object.cpp
@@ -4,7 +4,7 @@ using namespace shapegame;
 Object::Object(double x, double y) : pos(x, y) {}
 Object::Object(Position pos) : pos(pos) {}
 Object::~Object(){
-	std::cout <<"deleted: Object" <<std::endl;
+	// std::cout <<"deleted: Object" <<std::endl;
 }
 void Object::kill() {
 	this->canKill = true;
