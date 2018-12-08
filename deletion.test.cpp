8e34bed930eb8676eee844e4c7c53cb06fe18896
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-12-08 14:39:08 -0500

    the scene now takes a pointer and returns a pointer to the object added

--- deletion.test.cpp
+++ deletion.test.cpp
@@ -6,19 +6,27 @@ int main(){
 
 	Rectangle *rectVect[100];
 
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 50; i++) {
 		rectVect[i] = new Rectangle(5, 100, Position(i * 2, (i + 2) * 5), Color::BLUE);
-		game.scene->addChild(*rectVect[i]);
+		game.scene->addChild(rectVect[i]);
 	}
 
 
-	Timer *t = new Timer(1000, false, true, [=](){
-		for (int i = 0; i < 50; i++)
-			rectVect[i * 2]->kill();
-		std::cout << "hello" << std::endl;
-	}, false);
+	// Timer *t = new Timer(1000, false, true, [=](){
+	// 	for (int i = 0; i < 50; i++)
+	// 		rectVect[i * 2]->kill();
+	// 	std::cout << "hello" << std::endl;
+	// }, false);
 
-	game.scene->addChild(*t);
+	int i = 0;
+	// Object *o = game.scene->addChild(
+	game.scene->addChild(
+		new Timer(300, true, true, [=]() mutable {
+			rectVect[i]->kill();
+			i++;
+		}, false)
+	);
+	// o->kill();
 
 	game.run();
 }
