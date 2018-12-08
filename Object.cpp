e65982443bcaed319629eb18de2cb08b1c6cb458
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2018-12-08 14:15:21 -0500

    I am clearly out of the legal bounds.. maybe

--- Object.cpp
+++ Object.cpp
@@ -7,6 +7,10 @@ Object::~Object(){
 	// std::cout <<"deleted: Object" <<std::endl;
 }
 void Object::kill() {
+	this->onKill();
 	this->canKill = true;
+}
+void Object::onKill() {
+	std::cout << "kill called" << std::endl;
 }
 void Object::onRemove(){}
