93095b6f2ff2ff23c1c82277222efe7b685a4685
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-01-05 17:56:04 -0500

    seem to be properly calculating triangle height and width, but needs testing

--- include/Triangle.hpp
+++ include/Triangle.hpp
@@ -11,6 +11,7 @@ namespace shapegame {
         public:
             Point second; // second point of the triangle
             Point third; // third
+            Position collisionPosition;
             Triangle(Position first, Point second, Point third);
             Triangle(Position first, Point second, Point third, Color color);
             int getNumVerts();
