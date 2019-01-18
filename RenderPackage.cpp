351804bbbb2d1c1341850206a94c09d1d4987b91
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-01-17 20:30:58 -0500

    shape is now kept as a reference inside of renderObject

--- RenderPackage.cpp
+++ RenderPackage.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 #include "shapegame"
 
-shapegame::RenderPackage::RenderPackage(Shape *shape,
+shapegame::RenderPackage::RenderPackage(Shape &shape,
         GLRenderObject *glRenderObject): shape(shape) {
     this->glRenderObject = std::make_unique<GLRenderObject>(*glRenderObject);
 }
 bool shapegame::RenderPackage::updateDirty() {
-    if (this->shape->_dirty){
+    if (this->shape._dirty){
         // super waste. Don't throw away verticies, move them instead.
-        this->glRenderObject->verts = VertexGenerator::instance()->generate(*(this->shape));
-        this->shape->_dirty = false;
+        this->glRenderObject->verts = VertexGenerator::instance()->generate(this->shape);
+        this->shape._dirty = false;
         return true;
     }
     return false;
