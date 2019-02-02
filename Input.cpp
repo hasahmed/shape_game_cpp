4514339210b85ac086c085b9f1000b5f1de7e960
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-02-02 13:36:37 -0500

    sync

--- Input.cpp
+++ Input.cpp
@@ -1,4 +1,5 @@
 #include "shapegame"
+using namespace shapegame;
 using namespace shapegame::Input;
 bool Kb::down(Kb::Key key) {
 	GLFWwindow *w = Game::inst().getWindow()->window_handle;
@@ -6,4 +7,12 @@ bool Kb::down(Kb::Key key) {
 }
 bool Kb::up(Kb::Key key) {
 	return !Kb::down(key);
+}
+
+Position Mouse::pos() {
+	GLFWwindow *w = Game::inst().getWindow()->window_handle;
+	double x = 0;
+	double y = 0;
+	glfwGetCursorPos(w, &x, &y);
+	return Position(x, y);
 }
