2ac838d2a076047eec8aa6eda6c464b4c46b4ded
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-02-06 01:23:11 -0500

    sync

--- MultiShape.cpp
+++ MultiShape.cpp
@@ -6,11 +6,13 @@ MultiShape::MultiShape(Shape* mainShape, std::vector<Shape*>& shapes): Triangle(
 	for (Shape *s : shapes) {
 		this->shapes.push_back(s);
 	}
-	cout << *this << endl;
+	std::cout << "shapes.size(): " << shapes.size() << std::endl;
+	// cout << *this << endl;
 }
 void MultiShape::onAdd(){
-	std::cout << "added\n";
+	// std::cout << "shapes.size(): " << this->shapes.size() << std::endl;
 	for (Shape *s : this->shapes) {
+		std::cout << "a" << std::endl;
 		Game::inst().scene->addChild(s);
 	}
 }
--- main.test.cpp
+++ main.test.cpp
@@ -10,8 +10,8 @@ class TriangleEqualateral : public Triangle {
 		}
 	
 	void onAdd() override {
-		std::cout << "ADDDDEDDDDD\n";
-		std::cout << *this << std::endl;
+		// std::cout << "ADDDDEDDDDD\n";
+		// std::cout << *this << std::endl;
 	}
 };
 
@@ -21,21 +21,24 @@ int main() {
 	g.scene->addChild(new DebugKeyHandler());
 	auto x = new TriangleEqualateral(100, 100, Position(0, 200), Color::BLACK);
 	auto y = new TriangleEqualateral(100, 100, Position(100, 200), Color::RED);
-	auto z = new TriangleEqualateral(100, 100, Position(300, 200), Color::BLUE);
+	auto z = new TriangleEqualateral(100, 100, Position(200, 200), Color::GREEN);
+	auto a = new TriangleEqualateral(100, 100, Position(300, 200), Color::PINK);
 	// auto y = new TriangleEqualateral(10, 10, Position(100, 200), Color::BLACK);
 	// auto shapes = std::vector<Shape*> { x, y, z };
-	auto shapes = std::vector<Shape*> { y, z, x };
-	std::cout << shapes.size() << std::endl;
+	// auto shapes = std::vector<Shape*> { x, y, z, a };
+	auto shapes = std::vector<Shape*> { x };
+	// auto shapes = std::vector<Shape*> { y };
 	// g.scene->addChild(new TriangleEqualateral(100, 100, Position(100, 200), Color::BLACK));
 	// g.scene->addChild(new TriangleEqualateral(10, 100, Position(300, 200), Color::GREEN));
-	// auto m = new MultiShape(x, shapes);
+	auto m = new MultiShape(x, shapes);
 
 	// std::cout << "M" << std::endl;
 	// std::cout << *m;
-	// g.scene->addChild(m);
-	g.scene->addChild(x);
-	g.scene->addChild(y);
-	g.scene->addChild(z);
+	g.scene->addChild(m);
+	// g.scene->addChild(x);
+	// g.scene->addChild(y);
+	// g.scene->addChild(z);
+	// g.scene->addChild(a);
 	// g.scene->addChild(y);
 	g.run();
 }
