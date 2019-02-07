916b605587f096ddec72cb302f132c5c15bc5f0b
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-02-06 21:46:47 -0500

    added some grays to colors

--- main.test.cpp
+++ main.test.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 using namespace shapegame;
 
-class TriangleEqualateral : public Triangle {
+class TriangleIsosceles : public Triangle {
 	public:
-	TriangleEqualateral(float width, float height, Position pos, Color color) :
+	TriangleIsosceles(float width, float height, Position pos, Color color) :
 		Triangle(Point(0, 0), Point(width / 2, -height), Point(width, 0), color) {
 			this->setPosition(pos);
 		}
@@ -18,18 +18,19 @@ class TriangleEqualateral : public Triangle {
 int main() {
 
 	Game g(1200, 700, "Multi Test");
+	g.scene->setBackgroundColor(Color::GRAY);
 	g.scene->addChild(new DebugKeyHandler());
-	auto x = new TriangleEqualateral(100, 100, Position(0, 200), Color::BLACK);
-	auto y = new TriangleEqualateral(100, 100, Position(100, 200), Color::RED);
-	auto z = new TriangleEqualateral(100, 100, Position(200, 200), Color::GREEN);
-	auto a = new TriangleEqualateral(100, 100, Position(300, 200), Color::PINK);
-	// auto y = new TriangleEqualateral(10, 10, Position(100, 200), Color::BLACK);
+	auto x = new TriangleIsosceles(100, 100, Position(0, 200), Color::BLACK);
+	auto y = new TriangleIsosceles(100, 100, Position(100, 200), Color::RED);
+	auto z = new TriangleIsosceles(100, 100, Position(200, 200), Color::GREEN);
+	auto a = new TriangleIsosceles(100, 100, Position(300, 200), Color::PINK);
+	// auto y = new TriangleIsosceles(10, 10, Position(100, 200), Color::BLACK);
 	// auto shapes = std::vector<Shape*> { x, y, z };
 	auto shapes = std::vector<Shape*> { x, y, z, a };
 	// auto shapes = std::vector<Shape*> { x };
 	// auto shapes = std::vector<Shape*> { y };
-	// g.scene->addChild(new TriangleEqualateral(100, 100, Position(100, 200), Color::BLACK));
-	// g.scene->addChild(new TriangleEqualateral(10, 100, Position(300, 200), Color::GREEN));
+	// g.scene->addChild(new TriangleIsosceles(100, 100, Position(100, 200), Color::BLACK));
+	// g.scene->addChild(new TriangleIsosceles(10, 100, Position(300, 200), Color::GREEN));
 	auto m = new MultiShape(x, shapes);
 
 	// std::cout << "M" << std::endl;
