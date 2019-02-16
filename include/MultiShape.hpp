b9be59818a746ae76dd3866eeb312a8d8af81571
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-02-16 01:46:39 -0500

    fixed multishapes by going back to dumb pointers

--- include/MultiShape.hpp
+++ include/MultiShape.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <vector>
-#include <memory>
 #include "Position.hpp"
 #include "Entity.hpp"
 
@@ -8,17 +7,14 @@ namespace shapegame {
 	class MultiShape : public Entity {
 		protected:
 		std::vector<Object*> shapes;
-		std::vector<std::unique_ptr<Object>> unAddedShapes;
 		public:
 		MultiShape(Object* controller, std::vector<Object*>& shapes);
 		MultiShape(Position pos);
 		virtual void addShape(Object* shape);
-		virtual void addShape(std::unique_ptr<Object> shape);
 		virtual void onAdd() override;
 		virtual void onKill() override;
 		virtual void setPosition(float x, float y) override;
 		virtual void setPosition(Point pos) override;
 		virtual void setZOrder(int zOrder) override;
-		// virtual void translate(float x, float y);
 	};
 }
