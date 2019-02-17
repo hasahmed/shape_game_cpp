6e24108ac661780498de0ee1a983b3854c7cc528
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-02-17 16:04:31 -0500

    sync

--- main.test.cpp
+++ main.test.cpp
@@ -16,7 +16,7 @@ class Box : public MultiShape {
 int main() {
 
 	Game g(1200, 700, "Multi Test");
-	// auto x = g.scene->addChildAs<DebugKeyHandler>(new DebugKeyHandler());
+	g.scene->addChildAs<DebugKeyHandler>(new DebugKeyHandler());
 		// g.scene->addChild(new Rectangle(10, 100, Position(100, 100), Color::BLACK));
 	// auto x = g.scene->addChildAs<Box>(new Box(Position(100, 100)));
 	auto x = g.scene->addChild(std::make_unique<Box>(Position(100, 100)));
