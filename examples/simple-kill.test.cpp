0c3b6cfd215c8e31a0195245c0b38537a204e115
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-02-17 16:21:33 -0500

    set-position errors

---
+++ examples/simple-kill.test.cpp
@@ -0,0 +1,25 @@
+#include "shapegame"
+#include <vector>
+using namespace shapegame;
+
+class Box : public MultiShape {
+	public:
+	Box(Position pos): MultiShape(pos) {
+		auto top = new Rectangle(100, 10, pos, Color::BLACK);
+		auto left = new Rectangle(10, 100, pos, Color::BLACK);
+
+		this->addShape(top);
+		this->addShape(left);
+	}
+};
+
+int main() {
+
+	Game g(1200, 700, "Multi Test");
+	g.scene->addChildAs<DebugKeyHandler>(new DebugKeyHandler());
+		// g.scene->addChild(new Rectangle(10, 100, Position(100, 100), Color::BLACK));
+	// auto x = g.scene->addChildAs<Box>(new Box(Position(100, 100)));
+	auto x = g.scene->addChild(std::make_unique<Box>(Position(100, 100)));
+	x->kill();
+	g.run();
+}
