1f1615419a8a55cbd76a808dab442672bf558d50
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-05-16 00:54:59 -0400

    Can still draw shapes

--- main.test.cpp
+++ main.test.cpp
@@ -8,5 +8,6 @@ int main() {
 	Game g(1200, 800, "Busy Highway");
 	g.scene->setBackgroundColor(Color::GRAY);
 	g.scene->addChild(std::make_unique<DebugKeyHandler>());
+	g.scene->addChild(std::make_unique<Triangle>(Position(100, 100), Point(500, 300), Point(50, 300)));
 	g.run();
 }
