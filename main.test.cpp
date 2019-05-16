58f683e0ed8ad25281a9795ce6f65bb87d54c41e
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-05-16 01:25:50 -0400

    Added multi-draw code

--- main.test.cpp
+++ main.test.cpp
@@ -9,5 +9,6 @@ int main() {
 	g.scene->setBackgroundColor(Color::GRAY);
 	g.scene->addChild(std::make_unique<DebugKeyHandler>());
 	g.scene->addChild(std::make_unique<Triangle>(Position(100, 100), Point(500, 300), Point(50, 300)));
+	g.scene->addChild(std::make_unique<Rectangle>(100, 100, Position(100, 500), Color::BLUE));
 	g.run();
 }
