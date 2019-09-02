37e86ccfd44a13985b18bfbde868b4ebd106da6f
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2019-09-01 21:24:51 -0400

    Added tool script for building and zipping in one step

---
+++ tools/build-and-zip.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+
+import platform
+import os
+import util
+
+
+util.cd_root()
+
+
+system = platform.system()
+dist_name = ""
+make_arg = "dist-"
+
+if system == "Darwin":
+    dist_name = "mac"
+elif system == "Linux":
+    dist_name = "linux"
+elif system == "Windows":
+    dist_name = "win"
+else:
+    print("Error. Don't know how to handle OS [%s]" % system)
+    exit()
+
+
+make_arg += dist_name
+try:
+    os.system("make %s" % make_arg)
+except(e):
+    print("There has been an error")
+    print(e)
+
+os.chdir('tools/')
+
+# lazy way of running code that wasn't built to be called
+# when a module is imported its code is run
+import zip_dist
---
+++ tools/zip_dist.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import zipfile
+import os
+import util
+
+util.cd_root()
+
+
+ARCH_NAME = 'shapegame.zip'
+ZIP_DIST = 'zip-dist'
+
+def create_archive():
+	with zipfile.ZipFile(ARCH_NAME, 'w') as dist_zip:
+		os.chdir(util.DIST_PATH)
+		for root, dirs, files in os.walk('.'): #every file in dist-libs
+			for file in files:
+				dist_zip.write(os.path.join(root, file), os.path.join('shapegame', root, file))
+		os.chdir('..')
+		os.chdir(util.INCLUDE)
+		for root, dirs, files in os.walk('.'): #every file in dist-libs
+			for file in files:
+				dist_zip.write(os.path.join(root, file), os.path.join('shapegame', util.INCLUDE, root, file))
+		os.chdir('..')
+		os.chdir(util.DEPS) #in deps
+		for root, dirs, files in os.walk('.'): #every file in deps
+			for file in files:
+				dist_zip.write(os.path.join(root, file), os.path.join('shapegame', util.DEPS, root, file))
+		os.chdir('../') #leave directory in same state as before
+
+create_archive()
+os.rename(ARCH_NAME, os.path.join(ZIP_DIST, ARCH_NAME))
